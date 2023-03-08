903ce2787adcda117f4b8eb8b81ac9bedcdf7e76
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-03-08 03:18:57 +0100

    try

--- repl/src/repl.ts
+++ repl/src/repl.ts
@@ -1,15 +1,18 @@
 import josmFsAdapter from "../../app/src/josmFsAdapter"
 //const testElem = document.querySelector("#test")
 import { Data, DataBase } from "josm"
+import clone from "circ-clone"
+
 
 console.log("start")
 
 
 let d = new DataBase({lel: 2})
+
 const init = josmFsAdapter("test3.json", d).then((e) => {
   console.log("log", e)
   
-  d({lel: "lelel"})
+  d({lel: undefined})
 })
 
 
