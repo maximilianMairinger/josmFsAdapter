47f77c97612d679f335c16817de17251acd035d7
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2023-06-05 15:48:41 +0200

    Feature: promise as fallback val & Bug fix: bug: plain function as fallback val caused error where it shouldnt

--- app/src/josmFsAdapter.ts
+++ app/src/josmFsAdapter.ts
@@ -9,9 +9,11 @@ import clone from "circ-clone"
 const exists = (filename: string) => fs.stat(filename).then(() => true).catch(() => false)
 
 export async function josmFsAdapter<DB extends Data<T> | DataBase<T>, T>(fsPath: string, dataOrDb: DB): Promise<DB extends Data<infer R> ? R : DB extends DataBase<infer R> ? R : never>
+export async function josmFsAdapter<T>(fsPath: string, initValue: () => Promise<T>): Promise<T extends object ? DataBase<T> : Data<T>>
 export async function josmFsAdapter<T>(fsPath: string, initValue: () => T): Promise<T extends object ? DataBase<T> : Data<T>>
+export async function josmFsAdapter<T>(fsPath: string, initValue: Promise<T>): Promise<T extends object ? DataBase<T> : Data<T>>
 export async function josmFsAdapter<T>(fsPath: string, initValue: T): Promise<T extends object ? DataBase<T> : Data<T>>
-export async function josmFsAdapter(fsPath: string, dbOrDataOrInits: Data<unknown> | DataBase<any> | unknown | (() => unknown)): Promise<any> {
+export async function josmFsAdapter(fsPath: string, dbOrDataOrInits: Data<unknown> | DataBase<any> | unknown | Promise<unknown> | (() => (unknown | Promise<unknown>))): Promise<any> {
   let dataOrDb: Data<unknown> | DataBase<any>
   let ret: any
 
@@ -27,18 +29,22 @@ export async function josmFsAdapter(fsPath: string, dbOrDataOrInits: Data<unknow
   let proms: Promise<any>[] = []
 
   if (dbOrDataOrInits[instanceTypeSym] === undefined) {
+
+    let givenDefaultVal = dbOrDataOrInits instanceof Function ? dbOrDataOrInits() : dbOrDataOrInits
     let rawData: string
     try {
       initData = parse(rawData = await fs.readFile(fsPath, "utf8"))
     }
     catch(e) {
       if (fileExists) if (rawData !== "") new Error("josmFsAdapter: fsPath exists, but is not a valid json")
-      initData = dbOrDataOrInits instanceof Function ? dbOrDataOrInits() : dbOrDataOrInits
+      initData = await givenDefaultVal
       await waitTillPath
       proms.push(writeToDisk(initData))
     }
 
-    if (typeof dbOrDataOrInits !== typeof initData) throw new Error(`josmFsAdapter: data on disk (typeof ${typeof initData}) is not the same type as init data (typeof ${typeof dbOrDataOrInits})`)
+    givenDefaultVal = await givenDefaultVal
+
+    if (typeof givenDefaultVal !== typeof initData) throw new Error(`josmFsAdapter: data on disk (typeof ${typeof initData}) is not the same type as init data (typeof ${typeof dbOrDataOrInits})`)
 
     ret = dataOrDb = typeof initData === "object" ? new DataBase(initData) : new Data(initData)
   }
